136ca0dcb45dabe41b920d285431620b592cc1a5
Krishna Sivakumar <krishnasivaprogrammer@gmail.com>
2022-01-19 12:59:41 +0530

    implemented basic functionality

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path
+import tasks.views
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # Add all your views here
+    path("tasks/", tasks.views.tasks_view),
+    path("tasks/add-task/", tasks.views.add_task_view),
+    path("tasks/delete-task/<int:task_index>", tasks.views.delete_task_view),
+    path("tasks/complete-task/<int:task_index>", tasks.views.complete_task_view),
+    path("completed/", tasks.views.completed_task_view),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,1 +1,31 @@
 # Add all your views here
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+current_tasks = []
+completed_tasks = []
+
+
+def tasks_view(request):
+    return render(request, "current.html", {"tasks": current_tasks})
+
+
+def add_task_view(request):
+    task_value = request.GET.get("task")
+    current_tasks.append(task_value)
+    return HttpResponseRedirect("/tasks/")
+
+
+def delete_task_view(request, task_index):
+    current_tasks.pop(task_index - 1)
+    return HttpResponseRedirect("/tasks/")
+
+
+def complete_task_view(request, task_index):
+    task = current_tasks.pop(task_index - 1)
+    completed_tasks.append(task)
+    return HttpResponseRedirect("/tasks/")
+
+
+def completed_task_view(request):
+    return render(request, "completed.html", {"tasks": completed_tasks})
