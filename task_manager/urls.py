1eb1ec49512ce7881060f16827f61dc3e20d0cf4
Krishna Sivakumar <krishnasivaprogrammer@gmail.com>
2022-01-19 14:53:01 +0530

    modified routes to pass tests

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -6,9 +6,9 @@ import tasks.views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("tasks/", tasks.views.tasks_view),
-    path("tasks/add-task/", tasks.views.add_task_view),
-    path("tasks/delete-task/<int:task_index>", tasks.views.delete_task_view),
-    path("tasks/complete-task/<int:task_index>", tasks.views.complete_task_view),
-    path("completed/", tasks.views.completed_task_view),
+    path("add-task/", tasks.views.add_task_view),
+    path("delete-task/<int:task_index>/", tasks.views.delete_task_view),
+    path("complete_task/<int:task_index>/", tasks.views.complete_task_view),
+    path("completed_tasks/", tasks.views.completed_task_view),
     path("all_tasks/", tasks.views.all_tasks_view),
 ]
